b0f70a12c207633b1dae84f61abb9f02b54749ad
aayushtiwarigithub <aayushattiwari@gmail.com>
2024-04-06 03:22:55 +0530

    added a file

--- src/main/java/com/example/designpatterns/builderpattern/App.java
+++ src/main/java/com/example/designpatterns/builderpattern/App.java
@@ -3,8 +3,8 @@ package com.example.designpatterns.builderpattern;
 public class App {
     
     public static void main(String[] args) {
-        Computer pc = Computer.Builder()
-                         .RAM("16 GB")
+        Computer pc = new Computer.Builder()
+                .RAM("16 GB")
                          .cpu("intel i5")
                          .storage("500 GB")
                          .build();
